6eb6b527e811689623579b209d7cfd09c1b2112c
Louis Moreau <Louis Moreau>
2016-10-17 10:06:54 +0200

    added .ino file

---
+++ SME_Demo.ino
@@ -0,0 +1,24 @@
+// ---------------------------------------------------------------------
+// This file is an example on how to use the SmartEverything to
+// send the data through Sigfox network
+//
+// Created: 17.10.2016
+// Author: Alex Bucknall & Louis Moreau
+// Code:
+// https://github.com/bucknalla
+// https://github.com/luisomoreau
+// ---------------------------------------------------------------------
+
+// ---------------------------------------------------------------------
+// Define
+// ---------------------------------------------------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
